9e034f6dfc248acd79b774d80732cf3acf62f4da
Andreas Kochmann <andreaskochmann85@gmail.com>
2019-04-19 18:34:05 +0200

    Create Todo.js

---
+++ TodoApp/TodoAppPhaseOne/components/Todo.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+    function Todo(){
+        return(
+            <div>
+                <ul>
+                    <li><input type="checkbox" value="One"  />One</li>
+                    <li><input type="checkbox" value="Two"  />Two</li>
+                    <li><input type="checkbox" value="Three"  />Three</li>
+                </ul>
+            </div>
+        )
+    }
+
+export default Todo
