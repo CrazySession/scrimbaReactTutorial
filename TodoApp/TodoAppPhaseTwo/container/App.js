5cb711e737f59d52b0b062d995c13008567e28b6
Andreas Kochmann <andreaskochmann85@gmail.com>
2019-04-20 14:32:00 +0200

    Create App.js

---
+++ TodoApp/TodoAppPhaseTwo/container/App.js
@@ -0,0 +1,21 @@
+/*
+Time to have fun styling! But first things first: 
+
+1. Change the input/p combo below to be a new component called <TodoItem />. <TodoItem /> (for now) will just have the same displayed data below (every todo item is the same) hardcoded inside of it. (We'll learn soon how to make the TodoItem more flexible)
+    
+2. Style up the page however you want! You're welcome to use regular CSS (in the CSS file) or inline styles, or both!
+*/
+
+import React from "react"
+
+import TodoItem from '../components/TodoItem'
+
+function App() {
+    return (
+        <div>
+            <TodoItem   />
+        </div>
+    )
+}
+
+export default App
